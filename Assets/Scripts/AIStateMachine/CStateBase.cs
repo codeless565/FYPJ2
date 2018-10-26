0b5720e2d54d91575df80c7a87f921cbdf11ba8a
Lim Pei Sheng <peisheng1@hotmail.com>
2018-10-26 14:58:34 +0800

    Addedd CStateBase and CStateMachine

---
+++ Assets/Scripts/AIStateMachine/CStateBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface CStateBase
+{
+    void EnterState();
+    void UpdateState();
+    void ExitState();
+
+    string StateID
+    { get; }
+}
