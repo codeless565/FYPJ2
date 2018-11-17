4fc99854e65db066108dcdac0c064e39b8cfa5d8
Lim Pei Sheng <peisheng1@hotmail.com>
2018-11-17 14:08:59 +0800

    Shifted CTRoomCoordinate to own script
    moved general codes for dungeon creation and CTRoomCoordinate, Direction into new script

--- Assets/Scripts/LevelGenerator/CCorridor.cs
+++ Assets/Scripts/LevelGenerator/CCorridor.cs
@@ -148,33 +148,3 @@ public class CCorridor
     }
 }
 
-
-/******************
- * General Codes
- ******************/
-
-public enum Direction
-{
-    NORTH, SOUTH, EAST, WEST, Size
-}
-
-[System.Serializable]
-public class IntRange
-{
-    public int m_Min;       // The minimum value in this range.
-    public int m_Max;       // The maximum value in this range.
-
-    // Constructor to set the values.
-    public IntRange(int min, int max)
-    {
-        m_Min = min;
-        m_Max = max;
-    }
-    
-    // Get a random value from the range.
-    public int Random
-    {
-        get { return UnityEngine.Random.Range(m_Min, m_Max); }
-    }
-}
-
