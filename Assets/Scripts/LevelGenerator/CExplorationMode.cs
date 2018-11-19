93620c8d5d0841af9d3b0ee4967e60ddc37d455e
Lim Pei Sheng <peisheng1@hotmail.com>
2018-11-19 18:31:33 +0800

    Game will run off CExploration script

---
+++ Assets/Scripts/LevelGenerator/CExplorationMode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CExplorationMode : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<CTBoardGenerator>().init();
+        player.GetComponent<CPlayer>().init();
+        player.transform.position = CTDungeon.Instance.Floors[CTDungeon.Instance.currentFloor].GetRooms()[0].CenterPoint();
+	}
+}
