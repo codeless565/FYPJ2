2b8b99b5d79d2238762c596502ffa87f2a4f75fc
Lim Pei Sheng <peisheng1@hotmail.com>
2018-11-23 15:01:02 +0800

    Created new Inventory Sytem
    Items will follow Interface IItem, CInventory will be use by player to control item removel and adding

---
+++ Assets/Scripts/Items/HPPotion.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPPotion : MonoBehaviour, IItem
+{
+    Sprite m_ItemSprite;
+
+    void Awake()
+    {
+        m_ItemSprite = GetComponent<SpriteRenderer>().sprite;
+    }
+
+    public bool UseItem(ref CStats _playerStats)
+    {
+        //if (_playerStats.HP > _PlayerStats.MaxHP)
+        //  return false;
+        _playerStats.HP += 10;
+        return true;
+    }
+
+    public string ItemKey
+    {
+        get
+        {
+            return "HPPOT";
+        }
+    }
+
+    public string ItemName
+    {
+        get
+        {
+            return "Health Potion";
+        }
+    }
+
+    public string Description
+    {
+        get
+        {
+            return "Recovers 10 HP upon use.";
+        }
+    }
+
+    public Sprite ItemSprite
+    {
+        get
+        {
+            return m_ItemSprite;
+        }
+    }
+}
