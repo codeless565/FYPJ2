0c50082e35838637474449ef6a2431df0431cf70
Lim Pei Sheng <peisheng1@hotmail.com>
2018-11-25 22:29:02 +0800

    Created Hotbar and ItemSlot script to work with panel

---
+++ Assets/Scripts/Items/CInventorySlots.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CInventorySlots : MonoBehaviour {
+
+    public int InventorySlots = 3;
+    GameObject[] itemslots;
+    
+    public void Start()
+    {
+        itemslots = new GameObject[InventorySlots];
+        for (int i = 0; i < InventorySlots; i++)
+        {
+            itemslots[i] = gameObject.transform.GetChild(i).transform.GetChild(0).gameObject;
+            Debug.Log(itemslots[i]);
+        }
+    }
+
+    public bool AddItem(CItemSlot _newItem)
+    {
+        bool skipSecondCheck = false;
+        int i = 0;
+        CSlotInfo emptySlot = null;
+
+        foreach (GameObject slot in itemslots)
+        {
+            i++;
+            CSlotInfo currSlot = slot.transform.GetComponent<CSlotInfo>();
+            if (currSlot.isEmpty())
+            {
+                emptySlot = currSlot;
+                break;
+            }
+            if (currSlot.isSameItem(_newItem))
+                return false;
+            if (i >= itemslots.Length)
+                skipSecondCheck = true;
+        }
+
+        if (!skipSecondCheck)
+            foreach (GameObject slot in itemslots)
+            {
+                CSlotInfo currSlot = slot.GetComponent<CSlotInfo>();
+                if (currSlot.isEmpty())
+                    continue;
+                if (currSlot.isSameItem(_newItem))
+                    return false;
+            }
+
+        if (emptySlot != null)
+        {
+            emptySlot.SetItemSlot(_newItem);
+            Debug.Log(_newItem.itemInfo.ItemName + " added to Hotbar.");
+            return true;
+        }
+
+        Debug.Log("Hotbar full");
+        return false;
+    }
+}
---
+++ Assets/Scripts/Items/CSlotInfo.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CSlotInfo : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    CItemSlot m_itemSlot;
+
+    public void Start()
+    {
+        GetComponent<Image>().sprite = null;
+        GetComponent<Image>().color = Color.gray;
+        transform.GetChild(0).GetComponent<Text>().text = "";
+        m_itemSlot = null;
+    }
+
+    public void Update()
+    {
+        if (m_itemSlot != null)
+        {
+            if (m_itemSlot.quantity <= 0)
+            {
+                GetComponent<Image>().sprite = null;
+                GetComponent<Image>().color = Color.gray;
+                transform.GetChild(0).GetComponent<Text>().text = "";
+                m_itemSlot = null;
+                return;
+            }
+            GetComponent<Image>().sprite = m_itemSlot.itemInfo.ItemSprite;
+            GetComponent<Image>().color = Color.white;
+            transform.GetChild(0).GetComponent<Text>().text = m_itemSlot.quantity.ToString();
+        }
+    }
+
+    public void SetItemSlot(CItemSlot _newItemInfo)
+    {
+        m_itemSlot = _newItemInfo;
+    }
+
+    public bool isEmpty()
+    {
+        if (m_itemSlot == null)
+            return true;
+
+        return false;
+    }
+
+    public bool isSameItem(CItemSlot _newItemInfo)
+    {
+        if (_newItemInfo.itemInfo.ItemKey == m_itemSlot.itemInfo.ItemKey)
+            return true;
+
+        return false;
+    }
+
+    /********************
+     * Drag Drop Control
+     ********************/
+    #region DragDrop
+    private bool isSwapping;
+    private GameObject otherSlot;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (isSwapping)
+        {
+            CItemSlot oldSlot = m_itemSlot;
+            m_itemSlot = otherSlot.GetComponent<CSlotInfo>().m_itemSlot;
+            otherSlot.GetComponent<CSlotInfo>().m_itemSlot = oldSlot;
+            isSwapping = false;
+            Debug.Log("Swapped");
+        }
+        else
+        {
+            if ((transform.localPosition - Vector3.zero).magnitude >= 50)
+            {
+                GetComponent<Image>().sprite = null;
+                GetComponent<Image>().color = Color.gray;
+                transform.GetChild(0).GetComponent<Text>().text = "";
+                m_itemSlot = null;
+                Debug.Log("item removed from hotbar");
+            }
+        }
+        Debug.Log("moved - " + (transform.localPosition - Vector3.zero).magnitude);
+
+        transform.localPosition = Vector3.zero;
+    }
+
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        if (_other.gameObject.GetComponent<CSlotInfo>())
+        {
+            isSwapping = true;
+            otherSlot = _other.gameObject;
+            Debug.Log("Swap detected");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isSwapping = false;
+        otherSlot = null;
+    }
+
+    #endregion
+}
