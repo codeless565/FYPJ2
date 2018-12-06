59d933349b28891bd71f4598a416e14c95d4b837
Lim Pei Sheng <peisheng1@hotmail.com>
2018-12-06 10:42:08 +0800

    Created IBuff and Debuff_Slow
    IBuff is the interface all buff and debuff will be using to be accessed by external scripts.
    Debuff_Slow is the test script for buff to check if thing work

---
+++ Assets/Scripts/BuffSystem/Debuff_Slow.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debuff_Slow : IBuff
+{
+    //Default stuff
+    bool m_active;
+    Sprite m_buffSprite;
+    GameObject m_Target;
+
+    //Buff members
+    float m_timer;
+    float m_deletedMoveSpeed;
+
+    public void Init(GameObject _target)
+    {
+        m_Target = _target;
+        m_active = true;
+        //m_buffSprite = (Sprite)Resources.Load("Buffs/Slow");
+
+        m_timer = 5.0f;
+        if (m_Target.GetComponent<CStats>() != null)
+        {
+            m_deletedMoveSpeed = m_Target.GetComponent<CStats>().MoveSpeed * 0.3f;
+            m_Target.GetComponent<CStats>().MoveSpeed -= m_deletedMoveSpeed;
+        }
+    }
+
+    public void UpdateBuff()
+    {
+        if (!m_active)
+            return;
+
+        m_timer -= Time.deltaTime;
+        if (m_timer <= 0)
+        {
+            DestoryBuff();
+            m_active = false;
+        }
+    }
+
+    public void DestoryBuff()
+    {
+        m_Target.GetComponent<CStats>().MoveSpeed += m_deletedMoveSpeed;
+    }
+
+    public void ResetBuff()
+    {
+        m_timer = 5.0f;
+    }
+
+    public bool Active
+    { get { return m_active; } }
+
+    public bool isDebuff
+    { get { return true; } }
+
+    public string BuffName
+    { get { return "Slow Motion"; } }
+
+    public Sprite BuffSprite
+    { get { return m_buffSprite; } }
+
+    public string BuffDescription
+    { get { return "Slows target's movement speed by 30% for 5 seconds."; } }
+
+}
---
+++ Assets/Scripts/BuffSystem/IBuff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public interface IBuff
+{
+    void Init(GameObject _target);
+    void UpdateBuff();
+    void DestoryBuff();
+    void ResetBuff();
+
+    bool Active
+    { get; }
+
+    bool isDebuff
+    { get; }
+
+    string BuffName
+    { get; }
+
+    Sprite BuffSprite
+    { get; }
+
+    string BuffDescription
+    { get; }
+}
