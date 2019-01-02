e34006a69e35889826da69cc553999a96556d6c6
Lim Pei Sheng <peisheng1@hotmail.com>
2019-01-02 13:56:11 +0800

    Created PathFinding state for testing

---
+++ Assets/Scripts/AIStateMachine/CStatePathTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CStatePathTest : IStateBase
+{
+    public GameObject m_GO;
+
+    //unique var
+    List<CPathNode> m_Pathing;
+
+    public string StateID
+    {
+        get { return "StatePathTest"; }
+    }
+
+    public GameObject GO
+    {
+        get { return m_GO; }
+    }
+
+    public CStatePathTest(GameObject _gameObj)
+    {
+        m_GO = _gameObj;
+        m_Pathing = new List<CPathNode>();
+    }
+
+    public void EnterState()
+    {
+        Debug.Log("CStatePath::EnterState() called");
+
+        List<CTRoom> rmList =  CTDungeon.Instance.Floors[CTDungeon.Instance.currentFloor].GetRooms();
+        int randomDestIndex = Random.Range(0, rmList.Count - 1);
+
+
+    }
+
+    public void UpdateState()
+    {
+        Debug.Log("CStatePath::UpdateState() called");
+    }
+
+    public void ExitState()
+    {
+        Debug.Log("CStatePath::ExitState() called");
+    }
+}
