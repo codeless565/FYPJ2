239512be9e22da4bef51a8ea34b0081ffd3086c9
Lim Pei Sheng <peisheng1@hotmail.com>
2019-01-02 17:46:59 +0800

    Created RoomDetector prefab
    detector for setting entity coord upon entering room

---
+++ Assets/Scripts/LevelGenerator/RoomCoordinateSetter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCoordinateSetter : MonoBehaviour {
+
+    CTRoomCoordinate m_roomCoord;
+
+	// Use this for initialization
+	void Start () {
+        if (m_roomCoord == null)
+            m_roomCoord = new CTRoomCoordinate(0, 0);
+    }
+
+    public void Init(CTRoomCoordinate _coord)
+    {
+        m_roomCoord = _coord;
+    }
+
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        if (_other.GetComponent<IEntity>() != null)
+        {
+            if (_other.GetComponent<IEntity>().roomCoordinate != null)
+            {
+                _other.GetComponent<IEntity>().roomCoordinate = m_roomCoord;
+            }
+            Debug.Log("Coordinate of " + " is " + _other.GetComponent<IEntity>().roomCoordinate.x + ", " + _other.GetComponent<IEntity>().roomCoordinate.y);
+        }
+    }
+}
