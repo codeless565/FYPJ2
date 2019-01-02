a8138dd9a2f157ee7d9305c93fc5c9c924d997af
Poh <nicholasrandomz@yahoo.com.sg>
2019-01-02 08:18:35 +0800

    Prestige System and Base class

---
+++ Assets/Scripts/PrestigeSystem/PrestigeBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface PrestigeBase {
+    //string GetName();
+
+    void AddPrestigeStats(ref CStats _playerStats);
+    void RemovePrestigeStats(ref CStats _playerStats);
+}
