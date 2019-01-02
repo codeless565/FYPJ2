29b1f7b4a27b92ff1921c39a25e3b811633e979e
Lim Pei Sheng <peisheng1@hotmail.com>
2019-01-02 17:49:01 +0800

    Updated StatePathTest

--- Assets/Scripts/AIStateMachine/CStatePathTest.cs
+++ Assets/Scripts/AIStateMachine/CStatePathTest.cs
@@ -7,7 +7,8 @@ public class CStatePathTest : IStateBase
     public GameObject m_GO;
 
     //unique var
-    List<CPathNode> m_Pathing;
+    Queue<CPathNode> m_Pathing;
+    CPathNode nextDest;
 
     public string StateID
     {
@@ -22,26 +23,55 @@ public class CStatePathTest : IStateBase
     public CStatePathTest(GameObject _gameObj)
     {
         m_GO = _gameObj;
-        m_Pathing = new List<CPathNode>();
+        m_Pathing = new Queue<CPathNode>();
+
+        EnterState();
     }
 
     public void EnterState()
     {
         Debug.Log("CStatePath::EnterState() called");
 
-        List<CTRoom> rmList =  CTDungeon.Instance.Floors[CTDungeon.Instance.currentFloor].GetRooms();
+        List<CTRoom> rmList = CTDungeon.Instance.Floors[CTDungeon.Instance.currentFloor].GetRooms();
         int randomDestIndex = Random.Range(0, rmList.Count - 1);
 
-
+        m_Pathing = CTDungeon.Instance.BFS_ToRoom(m_GO.GetComponent<IEntity>().roomCoordinate, rmList[randomDestIndex].coordinate);
+        if (m_Pathing != null)
+            nextDest = m_Pathing.Dequeue();
     }
 
     public void UpdateState()
     {
         Debug.Log("CStatePath::UpdateState() called");
+        if (nextDest == null)
+        {
+            newPathing();
+            nextDest = m_Pathing.Dequeue();
+        }
+        if ((nextDest.position - (Vector2)m_GO.transform.position).magnitude <= m_GO.GetComponent<IEntity>().GetStats().MoveSpeed * Time.deltaTime)
+        {
+            if (m_Pathing.Count <= 0)
+            {
+                newPathing();
+            }
+            nextDest = m_Pathing.Dequeue();
+        }
+
+        m_GO.transform.Translate((nextDest.position - (Vector2)m_GO.transform.position).normalized * m_GO.GetComponent<IEntity>().GetStats().MoveSpeed * Time.deltaTime);
     }
 
     public void ExitState()
     {
         Debug.Log("CStatePath::ExitState() called");
+    }
+
+    private void newPathing()
+    {
+        List<CTRoom> rmList = CTDungeon.Instance.Floors[CTDungeon.Instance.currentFloor].GetRooms();
+        int randomDestIndex = Random.Range(0, rmList.Count - 1);
+
+        m_Pathing = CTDungeon.Instance.BFS_ToRoom(m_GO.GetComponent<IEntity>().roomCoordinate, rmList[randomDestIndex].coordinate);
+        if (m_Pathing == null)
+            newPathing();
     }
 }
