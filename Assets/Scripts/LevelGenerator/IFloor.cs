d21c7f67364b602c36fa5efca1852cf8f76f3d1e
Lim Pei Sheng <peisheng1@hotmail.com>
2019-01-07 17:28:40 +0800

    Created IFloor Interface for Floor Class usage

---
+++ Assets/Scripts/LevelGenerator/IFloor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IFloor
+{
+    bool Generated
+    { get; }
+
+    string Name
+    { get; set; }
+
+    Vector2 StairsForward
+    { get; }
+
+    Vector2 StairsBack
+    { get; }
+
+    List<CTRoom> Rooms
+    { get; }
+
+    TileType[][] Tiles
+    { get; }
+
+    void InitNewLevel(int _floorNum, int _columns, int _rows, int _numRooms, int _gridSize, int _roomWidth, int _roomHeight, int _corridorLength);
+
+}
