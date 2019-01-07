b049db3355ce05e75243b87dad7d50dbbfaa749d
Lim Pei Sheng <peisheng1@hotmail.com>
2019-01-07 18:01:45 +0800

    Added DungeonEntrance prefab and script
    player will be able to transistion between town and dungeon

---
+++ Assets/Scripts/InteractableObjects/DungeonEntrance.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DungeonEntrance : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        if (_other.tag == "Player")
+        {
+            CTDungeon.Instance.currentFloor = 1;
+            SceneManager.LoadScene("DungeonLevel_Red");
+        }
+    }
+}
