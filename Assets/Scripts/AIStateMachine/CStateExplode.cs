867fb46ca2d10fed4ad6dca351095861efe976bc
Lim Pei Sheng <peisheng1@hotmail.com>
2019-01-14 15:13:19 +0800

    Added EnemyBoom

---
+++ Assets/Scripts/AIStateMachine/CStateExplode.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CStateExplode : IStateBase
+{
+    public string StateID
+    {
+        get { return "StateExplode"; }
+    }
+
+    GameObject m_GO;
+    public GameObject GO
+    {
+        get { return m_GO; }
+    }
+
+    public CStateExplode(GameObject _go)
+    {
+        m_GO = _go;
+    }
+
+    public void EnterState()
+    {
+    }
+
+    public void UpdateState()
+    {
+        foreach (GameObject Obj in m_GO.GetComponent<IAreaOfEffect>().TargetList)
+        {
+            CDamageCalculator.Instance.SendDamage(Obj, m_GO.GetComponent<IEnemy>().GetStats().HP * 100);
+            Debug.Log("Hit " + Obj.name + " with explosion!");
+        }
+
+        Object.Destroy(m_GO);
+    }
+
+    public void ExitState()
+    {
+    }
+}
---
+++ Assets/Scripts/AIStateMachine/CStateExplodeArmed.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CStateExplodeArmed : IStateBase
+{
+    public string StateID
+    {
+        get { return "StateAttack"; }
+    }
+
+    GameObject m_GO;
+    public GameObject GO
+    {
+        get { return m_GO; }
+    }
+
+    float m_CountdownTimer;
+    public CStateExplodeArmed(GameObject _go)
+    {
+        m_GO = _go;
+        m_CountdownTimer = 3.0f;
+    }
+
+    public void EnterState()
+    {
+        m_GO.GetComponent<IEnemy>().ResetAtkTimer();
+        m_CountdownTimer = 3.0f;
+    }
+
+    public void UpdateState()
+    {
+        m_CountdownTimer -= Time.deltaTime;
+        if (m_CountdownTimer <= 0)
+        {
+            m_GO.GetComponent<IEnemy>().StateMachine.SetNextState("StateExplode");
+        }
+
+        if (((Vector2)m_GO.GetComponent<IEnemy>().Target.transform.position - (Vector2)m_GO.transform.position).magnitude > 2)
+        {
+            m_GO.GetComponent<IEnemy>().StateMachine.SetNextState("StateChase");
+        }
+    }
+
+    public void ExitState()
+    {
+        m_CountdownTimer = 3.0f;
+    }
+}
