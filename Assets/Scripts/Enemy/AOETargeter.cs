f9811abec2f20daa599caeaef4849c804366bfba
Lim Pei Sheng <peisheng1@hotmail.com>
2019-01-14 15:12:38 +0800

    Added AOETargeter

---
+++ Assets/Scripts/Enemy/AOETargeter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AOETargeter : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        if (_other.GetComponent<IEntity>() != null)
+        {
+            GetComponentInParent<IAreaOfEffect>().TargetList.Add(_other.gameObject);
+            Debug.Log("Added " + _other.name + " to TargetList");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D _other)
+    {
+        if (_other.GetComponent<IEntity>() != null)
+        {
+            GetComponentInParent<IAreaOfEffect>().TargetList.Remove(_other.gameObject);
+            Debug.Log("Removed " + _other.name + " to TargetList");
+        }
+    }
+}
