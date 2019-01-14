c33c84c624df6e618bb72bf4fd8ea816bdb46093
nicholasrandomz <nicholasrandomz@yahoo.com.sg>
2019-01-14 14:16:57 +0800

    Added Quest Board

---
+++ Assets/Scripts/Quest/QBQuest.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QBQuest {
+    private static QBQuest instance;
+    private QBQuest()
+    {
+        RandomizeCurrQuest();
+    }
+
+    public static QBQuest Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new QBQuest();
+            return instance;
+        }
+    }
+
+    public QuestBase currQuest;
+
+    public void RandomizeCurrQuest()
+    {
+        currQuest = RandomizeQuest();
+    }
+	
+    public void UpdateQuest(QuestType _questtype, QuestTarget _questtarget)
+    {
+        // if slay
+        if (_questtype == QuestType.SLAY)
+        {
+            if (_questtarget == currQuest.QuestTarget)
+                currQuest.QuestAmount++;
+            if (currQuest.QuestAmount >= currQuest.QuestCompleteAmount)
+                currQuest.QuestComplete = true;
+        }
+        if(_questtype == QuestType.REACH)
+        {
+            if(CTDungeon.Instance.currentFloor == currQuest.QuestCompleteAmount)
+                currQuest.QuestComplete = true;
+        }
+    }
+
+    public QuestBase RandomizeQuest()
+    {
+        QuestBase newquest = new QuestBase();
+
+        QuestType randomType = (QuestType)Random.Range((int)QuestType.NONE + 1, (int)QuestType.TOTAL);
+
+        if (randomType == QuestType.SLAY)
+        {
+            QuestTarget randomTarget = (QuestTarget)Random.Range((int)QuestTarget.NONE + 1, (int)QuestTarget.TOTAL);
+
+            newquest = new QuestBase(randomType, randomTarget, Random.Range(1, 4) * 5);
+        }
+        else if (randomType == QuestType.REACH)
+        {
+            bool found = false;
+            int nextlevel = CTDungeon.Instance.currentFloor;
+            while (!found)
+            {
+                if (nextlevel % 10 == 0)
+                    found = true;
+                else
+                    nextlevel++;
+            }
+
+            newquest = new QuestBase(QuestType.REACH, nextlevel);
+        }
+
+        return newquest;
+    }
+
+}
---
+++ Assets/Scripts/Quest/QuestBase.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestBase {
+    QuestType m_QuestType;
+    QuestTarget m_QuestTarget;
+    float m_Amount;
+    float m_CompleteAmount;
+    bool m_QuestComplete;
+
+    public QuestType QuestType
+    {
+        get
+        {
+            return m_QuestType;
+        }
+        set
+        {
+            m_QuestType = value;
+        }
+    }
+
+    public QuestTarget QuestTarget
+    {
+        get
+        {
+            return m_QuestTarget;
+        }
+        set
+        {
+            m_QuestTarget = value;
+        }
+    }
+
+    public float QuestAmount
+    {
+        get
+        {
+            return m_Amount;
+        }
+        set
+        {
+            m_Amount = value;
+        }
+    }
+
+    public float QuestCompleteAmount
+    {
+        get
+        {
+            return m_CompleteAmount;
+        }
+        set
+        {
+            m_CompleteAmount = value;
+        }
+    }
+
+    public bool QuestComplete
+    {
+        get
+        {
+            return m_QuestComplete;
+        }
+        set
+        {
+            m_QuestComplete = value;
+        }
+    }
+
+    
+
+    public QuestBase()
+    {
+        m_QuestType = QuestType.NONE;
+        m_QuestTarget = QuestTarget.NONE;
+        m_Amount = 0;
+        m_CompleteAmount = 0;
+        m_QuestComplete = false;
+    }
+
+    // for slay quests
+    public QuestBase(QuestType _questtype, QuestTarget _questtarget, float _completeamount)
+    {
+        m_QuestType = _questtype;
+        m_QuestTarget = _questtarget;
+        m_Amount = 0;
+        m_CompleteAmount = _completeamount;
+        m_QuestComplete = false;
+    }
+
+    public QuestBase(QuestType _questtype, float _completeamount)
+    {
+        m_QuestType = _questtype;
+        m_QuestTarget = QuestTarget.NONE;
+        m_Amount = 0;
+        m_CompleteAmount = _completeamount;
+        m_QuestComplete = false;
+    }
+}
+
+
+public enum QuestType
+{
+    NONE,
+    SLAY,
+    REACH,
+    TOTAL,
+}
+
+public enum QuestTarget
+{
+    NONE,
+    NOISE,
+    TOTAL
+}
