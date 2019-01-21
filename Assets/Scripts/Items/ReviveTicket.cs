2cfa6077f4db1c0684899c1b8b7c670c181f5c42
Lim Pei Sheng <peisheng1@hotmail.com>
2019-01-21 17:55:14 +0800

    Added ReviveTicket

---
+++ Assets/Scripts/Items/ReviveTicket.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReviveTicket : MonoBehaviour, IItem
+{
+    Sprite m_ItemSprite;
+
+    void Awake()
+    {
+        m_ItemSprite = GetComponent<SpriteRenderer>().sprite;
+    }
+
+    public bool UseItem(ref CStats _playerStats)
+    {
+        Debug.Log("Revive item used!");
+        return true;
+    }
+
+    public string ItemKey
+    {
+        get
+        {
+            return "REVIVETIX";
+        }
+    }
+
+    public string ItemName
+    {
+        get
+        {
+            return "Revival Amulet";
+        }
+    }
+
+    public string Description
+    {
+        get
+        {
+            return "Allow you to revive upon taking fatal damage.";
+        }
+    }
+
+    public ItemType ItemType
+    {
+        get { return ItemType.Use; }
+    }
+
+    public Sprite ItemSprite
+    {
+        get
+        {
+            if (m_ItemSprite == null)
+                m_ItemSprite = GetComponent<SpriteRenderer>().sprite;
+            return m_ItemSprite;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D _other)
+    {
+        CPlayer player = _other.GetComponent<CPlayer>();
+        if (player != null)
+        {
+            player.m_InventorySystem.AddItem(this);
+            Destroy(gameObject);
+        }
+    }
+
+}
