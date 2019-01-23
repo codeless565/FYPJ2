c6ca5445493a0dc66af8eb653f5fbd227851b2a5
Lim Pei Sheng <peisheng1@hotmail.com>
2019-01-23 14:10:18 +0800

    Update CStateChangeRoom.cs
    AI will follow new pathing method due to the BFS of new pathnode. AI will now try to make way for others if it is stuck for awhile

--- Assets/Scripts/AIStateMachine/CStateChangeRoom.cs
+++ Assets/Scripts/AIStateMachine/CStateChangeRoom.cs
@@ -18,6 +18,9 @@ public class CStateChangeRoom : IStateBase
     Queue<CPathNode> m_Pathing;
     CPathNode nextDest;
 
+    Vector3 m_lastknowposition;
+    float m_blockedTime;
+
     public CStateChangeRoom(GameObject _go)
     {
         m_GO = _go;
@@ -37,35 +40,55 @@ public class CStateChangeRoom : IStateBase
         }
         else
             m_GO.GetComponent<IEnemy>().StateMachine.SetNextState("StateIdle");
+
+        m_lastknowposition = m_GO.gameObject.transform.position;
     }
 
     public void UpdateState()
     {
         if (nextDest == null || m_Pathing == null)
+        {
             m_GO.GetComponent<IEnemy>().StateMachine.SetNextState("StateIdle");
+            return;
+        }
+
+        if ((m_GO.transform.position - GameObject.FindGameObjectWithTag("Player").transform.position).magnitude <= 5)
+        {
+            Debug.Log("Dist to plyer = " + (m_GO.transform.position - GameObject.FindGameObjectWithTag("Player").transform.position).magnitude);
+            m_GO.GetComponent<IEnemy>().Target = GameObject.FindGameObjectWithTag("Player");
+            m_GO.GetComponent<IEnemy>().StateMachine.SetNextState("StateChase");
+        }
+
+        //Move to next point
+        Vector2 forwardVec = nextDest.position - (Vector2)m_GO.transform.position;
+
+        //Reaching next point in the this frame
+        if (forwardVec.magnitude <= m_GO.GetComponent<IEntity>().GetStats().MoveSpeed * Time.deltaTime)
+        {
+            m_GO.transform.Translate(forwardVec.normalized * m_GO.GetComponent<IEntity>().GetStats().MoveSpeed * Time.deltaTime);
+            if (m_Pathing.Count > 0) //still have more point till final point
+                nextDest = m_Pathing.Dequeue();
+            else //Reached
+                m_GO.GetComponent<IEnemy>().StateMachine.SetNextState("StatePatrol");
+        }
+        else
+            m_GO.transform.Translate(forwardVec.normalized * m_GO.GetComponent<IEntity>().GetStats().MoveSpeed * Time.deltaTime);
+
+        //Make way if it is blocked
+        if ((m_lastknowposition - m_GO.gameObject.transform.position).magnitude < 1)
+        {
+            m_blockedTime += Time.deltaTime;
+            if (m_blockedTime >= 1)
+            {
+                Vector2 makeleft = -Vector3.Cross(forwardVec, Vector3.forward).normalized;
+                m_GO.transform.Translate(makeleft * m_GO.GetComponent<IEntity>().GetStats().MoveSpeed * Time.deltaTime);
+                Debug.Log(m_GO.name +" making way: " + makeleft);
+            }
+        }
         else
         {
-            //Move to next point
-            Vector2 forwardVec = nextDest.position - (Vector2)m_GO.transform.position;
-
-            //Reaching next point in the this frame
-            if (forwardVec.magnitude <= m_GO.GetComponent<IEntity>().GetStats().MoveSpeed * Time.deltaTime)
-            {
-                m_GO.transform.Translate(forwardVec.normalized * m_GO.GetComponent<IEntity>().GetStats().MoveSpeed * Time.deltaTime);
-                if (m_Pathing.Count > 0) //still have more point till final point
-                    nextDest = m_Pathing.Dequeue();
-                else //Reached
-                    m_GO.GetComponent<IEnemy>().StateMachine.SetNextState("StatePatrol");
-            }
-            else
-                m_GO.transform.Translate(forwardVec.normalized * m_GO.GetComponent<IEntity>().GetStats().MoveSpeed * Time.deltaTime);
-
-            if ((m_GO.transform.position - GameObject.FindGameObjectWithTag("Player").transform.position).magnitude <= 5)
-            {
-                Debug.Log("Dist to plyer = " + (m_GO.transform.position - GameObject.FindGameObjectWithTag("Player").transform.position).magnitude);
-                m_GO.GetComponent<IEnemy>().Target = GameObject.FindGameObjectWithTag("Player");
-                m_GO.GetComponent<IEnemy>().StateMachine.SetNextState("StateChase");
-            }
+            m_lastknowposition = m_GO.gameObject.transform.position;
+            m_blockedTime = 0;
         }
     }
 
