d3a638e0c9632568bb85fa89b57dbe935ed34b3b
Lim Pei Sheng <peisheng1@hotmail.com>
2019-01-28 18:52:43 +0800

    Created SmithShop for WeaponSelector

---
+++ Assets/Scripts/MenuScripts/WeaponSelector.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponSelector : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Player;
+    CPlayer m_Player;
+
+    [SerializeField]
+    GameObject ConfirmationText;
+    Text m_CfmText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (Player == null)
+            Player = GameObject.FindGameObjectWithTag("Player");
+        m_Player = Player.GetComponent<CPlayer>();
+
+        m_CfmText = ConfirmationText.GetComponent<Text>();
+
+        gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_CfmText.text = "Current Instrument: " + m_Player.EquippedWeapon.Name;
+	}
+
+    public void OpenMenu()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ChangeWeaponToMelodica()
+    {
+        m_Player.EquippedWeapon = new Melodica();
+    }
+
+    public void ChangeWeaponToGuitar()
+    {
+        m_Player.EquippedWeapon = new Guitar();
+    }
+
+    public void ChangeWeaponToRecorder()
+    {
+        m_Player.EquippedWeapon = new Recorder();
+    }
+}
