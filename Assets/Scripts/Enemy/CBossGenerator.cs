d870f6570b4d8ff30dd0f017312e9db993e63786
Lim Pei Sheng <peisheng1@hotmail.com>
2019-01-28 17:55:41 +0800

    Created BossGenerator

---
+++ Assets/Scripts/Enemy/CBossGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CBossGenerator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject BossRed;
+    [SerializeField]
+    GameObject BossPink;
+
+    public void Init()
+    {
+        CTRoom BossRoom = CTDungeon.Instance.Floors[CTDungeon.Instance.currentFloor].Rooms[1];
+
+        GenerateinRoom(BossRoom);
+    }
+
+    private void GenerateinRoom(CTRoom _Room)
+    {
+        GameObject Monster;
+
+        if (CTDungeon.Instance.currentFloor == 25)
+        {
+            Monster = Instantiate(BossRed, _Room.CenterPoint, Quaternion.identity);
+            Monster.GetComponent<IEnemy>().Init(_Room.coordinate);
+        }
+        else if (CTDungeon.Instance.currentFloor == 50)
+        {
+            Monster = Instantiate(BossPink, _Room.CenterPoint, Quaternion.identity);
+            Monster.GetComponent<IEnemy>().Init(_Room.coordinate);
+        }
+    }
+}
